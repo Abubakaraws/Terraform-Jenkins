4b1bfaf962e59bea2f3ac25214c6ba5b051b28db
Abubakaraws <114202973+Abubakaraws@users.noreply.github.com>
2022-12-28 19:09:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-0cca134ec43cf708f" # us-west-2
+  ami           = "ami-0cca134ec43cf708f" # ap-south-1
   instance_type = "t2.micro"
   tags = {
       Name = "harry"
